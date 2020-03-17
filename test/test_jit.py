87f6497d9f7a2c6af680ca2ef64f263591434126
Jerry Zhang <jerryzh168@gmail.com>
2020-03-17 14:27:08 -0700

    [quant][graphmode] quantization support for view, transpose, contiguos
    Summary:
    
    Test Plan:
    python test/test_jit.py
    
    Reviewers:
    .
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: ed17c1f56c627b2d6c6a1abf9cfd335d80c34254
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34854

--- test/test_jit.py
+++ test/test_jit.py
@@ -1983,6 +1983,9 @@ graph(%input, %weight):
                 x = torch.min(x)
                 x = torch.mean(x)
                 x = x.reshape([-1])
+                x = x.view(-1)
+                x = x.transpose(1, 2)
+                x = x.contiguous()
                 x, y = torch.chunk(x, 2)
                 x = F.dropout(x)
                 x = self.dropout(x)
@@ -2006,6 +2009,9 @@ graph(%input, %weight):
                    .check("aten::min") \
                    .check("aten::mean") \
                    .check("aten::reshape") \
+                   .check("aten::view") \
+                   .check("aten::transpose") \
+                   .check("aten::contiguous") \
                    .check("aten::chunk") \
                    .check("aten::dropout") \
                    .check("aten::dropout") \
@@ -2019,6 +2025,9 @@ graph(%input, %weight):
                    .check("aten::min") \
                    .check("aten::mean") \
                    .check("aten::reshape") \
+                   .check("aten::view") \
+                   .check("aten::transpose") \
+                   .check("aten::contiguous") \
                    .check("aten::chunk") \
                    .check("aten::dropout") \
                    .check("aten::dropout") \
