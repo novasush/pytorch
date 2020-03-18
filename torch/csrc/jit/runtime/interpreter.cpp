a0d79376bdd86c8de86bd5402c25f38553f97f6d
pritam <pritam.damania@fb.com>
2020-03-18 10:21:24 -0700

    Respect dist autograd context in torch.jit._fork.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34360
    
    The distributed autograd context sets up a thread local context id
    which is used to perform appropriate book keeping and autograd recording of RPC
    functions in the forward pass.
    
    However, if we use torch.jit._fork within the distributed autograd context, the
    code executed within torch.jit._fork will lose this context since it is run in
    a separate JIT thread and the thread local is not set in that thread.
    
    To fix this problem, we pass in the distributed autograd context to
    torch.jit._fork similar to what we did in
    https://github.com/pytorch/pytorch/pull/16101.
    ghstack-source-id: 100390102
    
    Differential Revision: [D20301352](https://our.internmc.facebook.com/intern/diff/D20301352/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D20301352/)!

--- torch/csrc/jit/runtime/interpreter.cpp
+++ torch/csrc/jit/runtime/interpreter.cpp
@@ -1148,9 +1148,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
                     : state_(std::move(state)), stack_(std::move(stack)) {}
                 void operator()() {
                   at::launch(InterpreterContinuation(
-                      state_,
-                      std::move(stack_),
-                      autograd::GradMode::is_enabled()));
+                      state_, std::move(stack_), torch::getThreadLocalState()));
                 }
 
                private:
@@ -1275,7 +1273,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
             InterpreterContinuation continuation(
                 forked_interpreter,
                 Stack(stack.end() - inst.N, stack.end()),
-                autograd::GradMode::is_enabled());
+                torch::getThreadLocalState());
             drop(stack, inst.N);
             push(stack, forked_interpreter.getFuture());
             at::launch(std::move(continuation));
@@ -1312,7 +1310,7 @@ struct InterpreterStateImpl : c10::intrusive_ptr_target {
       const Frame& frame = frames[i];
       std::string previous_fn_name = frame.function->function_name_;
       size_t pc = frame.pc;
-      // CALL nodes have already advanced the pc, so 
+      // CALL nodes have already advanced the pc, so
       // undo that to report the call node
       if (i + 1 < frames.size()) {
         --pc;
@@ -1447,7 +1445,7 @@ InterpreterState::InterpreterState(
     : pImpl(std::move(pImpl_)) {}
 
 void InterpreterContinuation::operator()() {
-  autograd::AutoGradMode grad_mode(grad_mode_enabled);
+  torch::ThreadLocalStateGuard guard(thread_local_state);
   state.runAsync(stack);
 }
 } // namespace jit
